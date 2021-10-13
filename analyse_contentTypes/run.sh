911410fa7e1d1190ad0b662b52fe9d8caaa3c6bd
Rodrigo Casado <rodrigo.casado@contentful.com>
2021-10-13 13:22:36 +0200

    new binary for content anaylser

---
+++ analyse_contentTypes/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# docker run -d -p 80:80 docker/getting-started
+echo "crating image"
+docker build --tag ctf-docker .
+#fi
+
+docker run --rm -v $PWD/task:/ctfanalyser/task ctf-docker
+# docker rmi ctf-docker
